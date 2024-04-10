2b9f4bc8f4b31c28bc47e7bc5711a7dd03981c0c
ajk-psu <ajk7267@psu.edu>
2024-04-10 16:37:36 -0400

    Create classes

---
+++ ConsoleUI.java
@@ -0,0 +1,10 @@
+// Console UI Class
+// Responsible for drawing, creating, and destroying UI elements on console.
+
+public class ConsoleUI 
+{
+    public static void drawHeader()
+    {
+        System.out.println("[]================== First National Bank of Java ==================[]");
+    }
+}
